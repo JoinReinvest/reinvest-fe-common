baf135e71219604ab5c859440819eb48227dc0ed
Reinny Almonte <reinny@hookooekoo.co>
2023-03-22 15:34:06 -0400

    feat: updating form-schemas

---
+++ src/types/select-option.ts
@@ -0,0 +1,9 @@
+export interface SelectOption {
+  label: string;
+  value: string;
+};
+
+export type SelectOptions = SelectOption[];
+
+export type ExtendedSelectOption<Fields> = SelectOption & Fields;
+export type ExtendedSelectOptions<Fields> = ExtendedSelectOption<Fields>[];
