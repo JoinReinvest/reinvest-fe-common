5b156ea6580f295badab70c8b7aef1f60eb95fae
Reinny Almonte <reinny@hookooekoo.co>
2023-03-28 10:54:29 -0400

    feat: authentication code schema regex uses the hyphen separator

--- src/form-schemas.ts
+++ src/form-schemas.ts
@@ -31,7 +31,7 @@ export const formValidationRules = {
   referralCode: zod.string().regex(/^[a-zA-Z0-9]{3}-[a-zA-Z0-9]{3}$/, { message: 'Invalid referral code' }),
   date: zod.string({ required_error: requiredError }).regex(/^(\d{2})\/(\d{2})\/(\d{4})$/),
   phoneNumber: zod.string().regex(/^\d{10}$/, { message: 'Invalid phone number' }),
-  authenticationCode: zod.string({ required_error: requiredError }).regex(/^\d{6}$/, { message: 'Invalid authentication code' }),
+  authenticationCode: zod.string({ required_error: requiredError }).regex(/^\d{3}-\d{3}$/, { message: 'Invalid authentication code' }),
   socialSecurityNumber: zod.string().regex(/^(?!666|000|9\d{2})\d{3}-(?!00)\d{2}-(?!0{4})\d{4}$/, { message: 'Invalid Social Security Number' }),
   ein: zod.string().regex(/^[0-9]{3}-[0-9]{6}/, { message: 'Invalid EIN' }),
 
