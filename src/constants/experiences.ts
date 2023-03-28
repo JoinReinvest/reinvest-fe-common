90ccd1c7d6ff6c492ff319369238cef5ddc8661a
adammalej <adammalej1@gmail.com>
2023-03-28 16:23:41 +0200

    feat: upgrade graphql mutations

--- src/constants/experiences.ts
+++ src/constants/experiences.ts
@@ -1,9 +1,11 @@
-import { Experience } from "types/graphql";
-import { SelectOptions } from "types/select-option";
+import { Experience } from 'types/graphql';
+import { SelectOptions } from 'types/select-option';
 
-export const EXPERIENCES_AS_OPTIONS: SelectOptions = [
+export const EXPERIENCES_AS_VALUES: SelectOptions = [
   { title: 'No Experience', value: Experience.NoExperience },
   { title: 'Some Experience', value: Experience.SomeExperience },
   { title: 'Very Experienced', value: Experience.VeryExperienced },
   { title: 'Expert', value: Experience.Expert },
 ];
+
+export const ACCOUNT_TYPES_AS_OPTIONS = [Experience.NoExperience, Experience.SomeExperience, Experience.VeryExperienced, Experience.Expert] as const;
