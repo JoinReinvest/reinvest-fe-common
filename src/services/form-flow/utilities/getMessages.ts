db5850343e2cc59ecdd10cb6006e17360dbdb935
AdamHKEK <126069460+AdamHKEK@users.noreply.github.com>
2023-04-04 11:07:24 +0200

    Ria 649 (#19)
    * feat: add error response interface
    
    * feat: create get message function and error mapper
    
    * refactor: change name
    
    ---------
    
    Co-authored-by: adammalej <adammalej1@gmail.com>

---
+++ src/services/form-flow/utilities/getMessages.ts
@@ -0,0 +1,21 @@
+import { ErrorResponse } from '../../queries/interfaces';
+import { MessageMapper, messageMapper } from '../../../constants/messageMapper';
+
+export const getMessages = (error: ErrorResponse) => {
+  const { response } = error;
+  const { errors } = response;
+
+  const messages = errors.map(error => {
+    const extension = error.extensions;
+
+    if (!extension.details) {
+      return error.message;
+    }
+
+    return extension.details.map(detail => {
+      return `${detail.field} ${messageMapper[detail.type as keyof MessageMapper]}`;
+    });
+  });
+
+  return messages.flat();
+};
