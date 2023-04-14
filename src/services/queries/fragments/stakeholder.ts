a9efba7da1b34415d74fbe8bb9c09547d22fe5d9
adammalej <adammalej1@gmail.com>
2023-04-14 10:37:34 +0200

    fix: remove fragments from stakeholder

--- src/services/queries/fragments/stakeholder.ts
+++ src/services/queries/fragments/stakeholder.ts
@@ -1,13 +1,9 @@
 import { gql } from 'graphql-request';
-import { AddressFragment } from './address';
 import { DomicileFragment } from './domicile';
-import { DocumentFileLinkIdFragment } from './documentFileLinkId';
 import { PersonNameTypeFragment } from './personNameType';
 
 export const StakeholderFragment = gql`
-  ${AddressFragment}
   ${DomicileFragment}
-  ${DocumentFileLinkIdFragment}
   ${PersonNameTypeFragment}
   fragment StakeholderFragment on Stakeholder {
     id
