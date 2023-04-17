ad2780ea550386d3a8ca15f8d63768e6576ac75a
adammalej <adammalej1@gmail.com>
2023-04-17 13:25:43 +0200

    feat: change values

--- src/constants/corporation.ts
+++ src/constants/corporation.ts
@@ -1,6 +1,6 @@
 import { SelectOptions } from "../types/select-option";
 
-export const CORPORATION_ANNUAL_REVENUES = ['< $XX, XXX', '$XX - XX,XXX', '$XX,XXX - $XM', '$XM - $XMM', '$XMM+'] as const;
+export const CORPORATION_ANNUAL_REVENUES = ['Under $10K', '$10K-$99.9K', '$100K-$999.9M', '$1M-$9.99M', '$10M+'] as const;
 
 export type CorporationAnnualRevenue = (typeof CORPORATION_ANNUAL_REVENUES)[number];
 
@@ -9,7 +9,7 @@ export const CORPORATION_ANNUAL_REVENUE_AS_OPTIONS: SelectOptions = CORPORATION_
   value,
 }));
 
-export const CORPORATION_NUMBER_OF_EMPLOYEES = ['<XX', 'XX - XXX', 'XXX - XXXX', 'XXXX - XX,XXX', 'XX,XXX +'] as const;
+export const CORPORATION_NUMBER_OF_EMPLOYEES = ['Under 50', '50-500', '500+'] as const;
 
 export type CorporationNumberOfEmployees = (typeof CORPORATION_NUMBER_OF_EMPLOYEES)[number];
 
