440ba777cec0e239844593692588bf9ec6adc4e2
adammalej <adammalej1@gmail.com>
2023-04-19 14:09:50 +0200

    fix: change from milion to thousand

--- src/constants/corporation.ts
+++ src/constants/corporation.ts
@@ -1,6 +1,6 @@
 import { SelectOptions } from "../types/select-option";
 
-export const CORPORATION_ANNUAL_REVENUES = ['Under $10K', '$10K-$99.9K', '$100K-$999.9M', '$1M-$9.99M', '$10M+'] as const;
+export const CORPORATION_ANNUAL_REVENUES = ['Under $10K', '$10K-$99.9K', '$100K-$999.9K', '$1M-$9.99M', '$10M+'] as const;
 
 export type CorporationAnnualRevenue = (typeof CORPORATION_ANNUAL_REVENUES)[number];
 
