ae0a621f68924516858e34e43e065a8d6ad84b8f
Reinny Almonte <reinny@hookooekoo.co>
2023-04-21 12:50:44 -0400

    chore: deprecating strings utility

--- src/utilities/strings.ts
+++
@@ -1,7 +0,0 @@
-export function capitalizeFirstLetter(value: string | undefined | null) {
-  if (!value || typeof value !== 'string') {
-    return '';
-  }
-
-  return value.charAt(0).toUpperCase() + value.slice(1).toLowerCase();
-}
