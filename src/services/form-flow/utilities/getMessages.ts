ea50ad699d4e70f7117bb3a5315c7cc1d1436278
adammalej <adammalej1@gmail.com>
2023-04-26 11:55:12 +0200

    fix: add uniq message for ein

--- src/services/form-flow/utilities/getMessages.ts
+++ src/services/form-flow/utilities/getMessages.ts
@@ -13,8 +13,14 @@ export const getMessages = (error: ErrorResponse) => {
     }
 
     return extension.details.map(detail => {
-      if(detail.field === 'ssn' && detail.type === ERROR_CODES.NOT_UNIQUE) {
+      const { field, type} = detail;
+
+      if(field === 'ssn' && type === ERROR_CODES.NOT_UNIQUE) {
         return 'SSN is not unique';
+      }
+
+      if(field === 'ein' && type === ERROR_CODES.NOT_UNIQUE) {
+        return 'EIN is not unique';
       }
 
       return 'Something went wrong, please refresh the page'
