a992961c7b5a9ecbc5a11345b93ba16d0a44ee24
Reinny Almonte <reinny@hookooekoo.co>
2023-05-02 10:22:13 +0200

    feat: defining date format for investment flow

--- src/constants/date-formats.ts
+++ src/constants/date-formats.ts
@@ -1,2 +1,3 @@
 export const DATE_FORMAT = 'MM/DD/YYYY';
 export const API_DATE_FORMAT = 'YYYY-MM-DD';
+export const INVESTMENT_DATE_FORMAT = 'MMMM D, YYYY';
--- src/utilities/dates.ts
+++ src/utilities/dates.ts
@@ -1,5 +1,8 @@
-import dayjs from "dayjs";
-import { DATE_FORMAT, API_DATE_FORMAT } from '../constants/date-formats';
+import dayjs from 'dayjs';
+import pluginIsToday from 'dayjs/plugin/isToday';
+import { API_DATE_FORMAT, DATE_FORMAT, INVESTMENT_DATE_FORMAT } from '../constants/date-formats';
+
+dayjs.extend(pluginIsToday);
 
 export const formatDateForApi = (date: Date | string) => {
   return dayjs(date, DATE_FORMAT).format(API_DATE_FORMAT);
@@ -7,8 +10,16 @@ export const formatDateForApi = (date: Date | string) => {
 
 export const formatDateFromApi = (date: Date | string) => {
   return dayjs(date, API_DATE_FORMAT).format(DATE_FORMAT);
-}
+};
 
 export const isDateFromApi = (date: Date | string): boolean => {
   return dayjs(date, API_DATE_FORMAT, true).isValid();
-}
+};
+
+export const formatDateForInvestmentDisplay = (date: Date) => {
+  return dayjs(date).format(INVESTMENT_DATE_FORMAT);
+};
+
+export const isToday = (date: Date) => {
+  return dayjs(date).isToday();
+};
