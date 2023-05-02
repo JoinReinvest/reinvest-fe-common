18ffc230c43c43c26e8f31deb1f5e50445f3036f
Reinny Almonte <reinny@hookooekoo.co>
2023-05-02 10:22:35 +0200

    feat: defining constants for investment amounts

---
+++ src/constants/investment-amounts.ts
@@ -0,0 +1,6 @@
+export const INVESTMENT_PRESET_AMOUNTS = [
+  { value: '500', label: '$500' },
+  { value: '1000', label: '$1,000' },
+  { value: '5000', label: '$5,000' },
+  { value: '10000', label: '$10K' },
+];
