0db6d191d336bcd3090799ebbf60c0930379d799
adammalej <adammalej1@gmail.com>
2023-05-11 13:23:40 +0200

    feat: add investment summary query

---
+++ src/services/queries/getInvestmentSummary.ts
@@ -0,0 +1,39 @@
+import { useQuery, UseQueryOptions } from '@tanstack/react-query';
+import { gql, GraphQLClient } from 'graphql-request';
+import { Query } from '../../types/graphql';
+import { UseApiQueryWithParams } from './interfaces';
+import { UsdFragment } from './fragments/usd';
+
+type Hook = UseApiQueryWithParams<'getInvestmentSummary', { investmentId: string, config: UseQueryOptions}>;
+
+const getInvestmentSummaryQuery = gql`
+  ${UsdFragment}
+  query getInvestmentSummary($investmentId: ID!) {
+    getInvestmentSummary(investmentId: $investmentId) {
+      id
+      tradeId
+      createdAt
+      amount {
+        ...UsdFragment
+      }
+      status
+      investmentFees {
+        ...UsdFragment
+      }
+      subscriptionAgreementId
+    }
+  }
+`;
+
+export const useGetInvestmentSummary: Hook = (getApiClient, {investmentId, ...config}) =>
+  useQuery<Query['getInvestmentSummary']>({
+    queryKey: ['getInvestmentSummary'],
+    queryFn: async () => {
+      const api = await getApiClient() as GraphQLClient;
+
+      const { getInvestmentSummary } = await api.request<Query>(getInvestmentSummaryQuery, {investmentId});
+
+      return getInvestmentSummary;
+    },
+    ...config
+  });
