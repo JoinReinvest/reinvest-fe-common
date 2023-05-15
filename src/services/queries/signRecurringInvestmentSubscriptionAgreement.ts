aad7fa3a8c5447dcb34eb1ff98ff11bcaf225477
adammalej <adammalej1@gmail.com>
2023-05-15 10:54:42 +0200

    feat: add signRecurringInvestmentSubscriptionAgreement mutation

---
+++ src/services/queries/signRecurringInvestmentSubscriptionAgreement.ts
@@ -0,0 +1,25 @@
+import { useMutation } from '@tanstack/react-query';
+import { gql, GraphQLClient } from 'graphql-request';
+
+import { Mutation } from '../../types/graphql';
+import { UseApiMutationWithParams } from './interfaces';
+
+type Parameters = { accountId: string };
+type Hook = UseApiMutationWithParams<'signRecurringInvestmentSubscriptionAgreement', Parameters>;
+
+const signRecurringInvestmentSubscriptionAgreementMutation = gql`
+  mutation signRecurringInvestmentSubscriptionAgreement($accountId: ID!) {
+    signRecurringInvestmentSubscriptionAgreement(accountId: $accountId)
+  }
+`;
+
+export const useSignRecurringInvestmentSubscriptionAgreement: Hook = (getApiClient) =>
+  useMutation({
+    mutationFn: async (input) => {
+      const api = await getApiClient() as GraphQLClient;
+
+      const { initiateRecurringInvestment } = await api.request<Mutation>(signRecurringInvestmentSubscriptionAgreementMutation, { ...input });
+
+      return initiateRecurringInvestment;
+    },
+  });
