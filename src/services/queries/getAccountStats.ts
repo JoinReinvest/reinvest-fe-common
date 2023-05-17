e1eed1b1af1f0eaa2422bca48107f2a4caac8001
Reinny Almonte <126076121+reinny-hkek@users.noreply.github.com>
2023-05-17 20:47:00 +0200

    feat: defining getAccountStats query (#102)

---
+++ src/services/queries/getAccountStats.ts
@@ -0,0 +1,36 @@
+import { useQuery, UseQueryOptions } from '@tanstack/react-query';
+import { gql } from 'graphql-request';
+
+import { Query, QueryGetAccountStatsArgs } from '../../types/graphql';
+import { UseApiQueryWithParams } from './interfaces';
+import { AccountStatsFragment } from './fragments/account-stats';
+
+type Parameters = QueryGetAccountStatsArgs & { config?: UseQueryOptions }
+type Hook = UseApiQueryWithParams<'getAccountStats', Parameters>;
+
+export const getAccountStatsQuery = gql`
+  ${AccountStatsFragment}
+
+  query getAccountStats($accountId: String!) {
+    getAccountStats(accountId: $accountId) {
+      ...AccountStatsFragment
+    }
+  }
+`;
+
+export const useGetAccountStats: Hook = (getApiClient, { accountId, ...config }) => useQuery({
+  queryKey: ["getAccountStats", accountId],
+  queryFn: async () => {
+    const api = await getApiClient();
+
+    if (!api) {
+      return null;
+    }
+
+    const { getAccountStats } = await api.request<Query>(getAccountStatsQuery, { accountId });
+
+    return getAccountStats;
+  },
+  enabled: !!accountId,
+  ...config
+});
