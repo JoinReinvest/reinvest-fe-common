a0e88a543445cbf38a54af24fe7b9faa5975e114
reinny-hkek <reinny@hookooekoo.co>
2023-05-17 14:04:31 +0200

    refactor: conditionally adding decimal points on maskCurrency utility

--- src/utilities/currency.ts
+++ src/utilities/currency.ts
@@ -1,1 +1,13 @@
-export const formatToDolars = (value: number) => `$${value.toFixed(2)}`;
+interface Options {
+  addDecimalPoints?: boolean;
+}
+
+export const maskCurrency = (value: number | string, options?: Options) => {
+  const addDecimalPoints = !!options?.addDecimalPoints;
+  const numericValue = typeof value === 'string' ? parseFloat(value) : value;
+
+  const maybeValueWithDecimalPoints = addDecimalPoints ? numericValue.toFixed(2) : numericValue.toString();
+
+  return `${maybeValueWithDecimalPoints}`
+};
+
