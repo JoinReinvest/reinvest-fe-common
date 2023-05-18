ab1d35ba4114b1beaa7aeba6299110237bc10076
Reinny Almonte <126076121+reinny-hkek@users.noreply.github.com>
2023-05-18 13:33:44 +0200

    re-using recurring investment frequency type from API + deprecating date utilities (#104)
    * chore: deprecating dates utilities for handling recurring investment inverval dates
    
    * feat: using interface for recurring investment frequency from api

--- src/constants/recurring-investment-intervals.ts
+++ src/constants/recurring-investment-intervals.ts
@@ -1,41 +1,36 @@
-export enum RecurringInvestmentInterval {
-  WEEKLY = 'weekly',
-  BI_WEEKLY = 'bi-weekly',
-  MONTHLY = 'monthly',
-  QUARTERLY = 'quarterly',
-}
+import { RecurringInvestmentFrequency } from '../types/graphql';
 
-export const RECURRING_INVESTMENT_INTERVALS_WITHIN_MONTH = [RecurringInvestmentInterval.WEEKLY, RecurringInvestmentInterval.BI_WEEKLY];
+export const RECURRING_INVESTMENT_INTERVALS_WITHIN_MONTH = [RecurringInvestmentFrequency.Weekly, RecurringInvestmentFrequency.BiWeekly];
 
-export const RECURRING_INVESTMENT_INTERVAL_LABELS = new Map<RecurringInvestmentInterval, string>([
-  [RecurringInvestmentInterval.WEEKLY, 'Weekly'],
-  [RecurringInvestmentInterval.BI_WEEKLY, 'Bi-Weekly'],
-  [RecurringInvestmentInterval.MONTHLY, 'Monthly'],
-  [RecurringInvestmentInterval.QUARTERLY, 'Quaterly'],
+export const RECURRING_INVESTMENT_INTERVAL_LABELS = new Map<RecurringInvestmentFrequency, string>([
+  [RecurringInvestmentFrequency.Weekly, 'Weekly'],
+  [RecurringInvestmentFrequency.BiWeekly, 'Bi-Weekly'],
+  [RecurringInvestmentFrequency.Monthly, 'Monthly'],
+  [RecurringInvestmentFrequency.Quarterly, 'Quaterly'],
 ]);
 
 export const RECURRING_INVESTMENT_INTERVALS = [
   {
-    title: `${RECURRING_INVESTMENT_INTERVAL_LABELS.get(RecurringInvestmentInterval.WEEKLY)}`,
-    value: RecurringInvestmentInterval.WEEKLY,
+    title: `${RECURRING_INVESTMENT_INTERVAL_LABELS.get(RecurringInvestmentFrequency.Weekly)}`,
+    value: RecurringInvestmentFrequency.Weekly,
   },
   {
-    title: `${RECURRING_INVESTMENT_INTERVAL_LABELS.get(RecurringInvestmentInterval.BI_WEEKLY)}`,
-    value: RecurringInvestmentInterval.BI_WEEKLY,
+    title: `${RECURRING_INVESTMENT_INTERVAL_LABELS.get(RecurringInvestmentFrequency.BiWeekly)}`,
+    value: RecurringInvestmentFrequency.BiWeekly,
   },
   {
-    title: `${RECURRING_INVESTMENT_INTERVAL_LABELS.get(RecurringInvestmentInterval.MONTHLY)}`,
-    value: RecurringInvestmentInterval.MONTHLY,
+    title: `${RECURRING_INVESTMENT_INTERVAL_LABELS.get(RecurringInvestmentFrequency.Monthly)}`,
+    value: RecurringInvestmentFrequency.Monthly,
   },
   {
-    title: `${RECURRING_INVESTMENT_INTERVAL_LABELS.get(RecurringInvestmentInterval.QUARTERLY)}`,
-    value: RecurringInvestmentInterval.QUARTERLY,
+    title: `${RECURRING_INVESTMENT_INTERVAL_LABELS.get(RecurringInvestmentFrequency.Quarterly)}`,
+    value: RecurringInvestmentFrequency.Quarterly,
   },
 ];
 
 export const RECURRING_INVESTMENT_INTERVAL_VALUES = [
-  RecurringInvestmentInterval.WEEKLY,
-  RecurringInvestmentInterval.BI_WEEKLY,
-  RecurringInvestmentInterval.MONTHLY,
-  RecurringInvestmentInterval.QUARTERLY,
+  RecurringInvestmentFrequency.Weekly,
+  RecurringInvestmentFrequency.BiWeekly,
+  RecurringInvestmentFrequency.Monthly,
+  RecurringInvestmentFrequency.Quarterly,
 ] as const;
--- src/utilities/dates.ts
+++ src/utilities/dates.ts
@@ -58,25 +58,3 @@ export function formatDateForNotification(dateFromApi: string) {
 
   return date.format(NotificationsDateFormats.PREVIOUS_YEAR);
 }
-
-export function getWeekOfMonth(date: Dayjs) {
-  const startOfMonth = date.startOf('month');
-  const weekOfMonth = date.add(1, 'week').week() - startOfMonth.week();
-
-  return weekOfMonth;
-}
-
-export function countWeeksInMonth(date: Dayjs) {
-  const month = date.month();
-  const startOfMonth = date.startOf('month');
-  const endOfMonth = date.endOf('month');
-
-  const weeksInMonth = endOfMonth.week() - startOfMonth.week() + 1;
-
-  // If the start of the month is in the previous year, we should not count the week that overlaps with the previous year.
-  if (startOfMonth.week() === 1 && startOfMonth.year() !== month) {
-    return weeksInMonth - 1;
-  }
-
-  return weeksInMonth;
-}
