60df280466257a1451e1870d6af20e18495041fe
Michal Gasiorek <michal@hookooekoo.co>
2023-05-22 13:50:08 +0200

    RIA-1142: update investments amount and limits

--- src/constants/investment-limits.ts
+++ src/constants/investment-limits.ts
@@ -1,12 +1,15 @@
-import { AccountType } from "../types/graphql";
+import { AccountType } from '../types/graphql';
 
-export const MINIMUM_RECURRING_INVESTMENT_AMOUNT = 100;
-export const MINIMUM_INVESTMENT_AMOUNT = 500;
-export const MAXIMUM_INVESTMENT_AMOUNT = 100_000;
+export const ONE_TIME_INVESTMENT_MIN_AMOUNT = 500;
+export const ONE_TIME_CORPORATION_MIN_AMOUNT = 1000;
+export const ONE_TIME_INVESTMENT_MAX_AMOUNT = 100000;
+
+export const RECURRING_INVESTMENT_MAX_AMOUNT = 5000;
+export const RECURRING_INVESTMENT_MIN_AMOUNT = 100;
 
 export const MINIMUM_INVESTMENT_AMOUNT_BY_ACCOUNT_TYPE = new Map<AccountType, number>([
-  [AccountType.Individual, 1000],
-  [AccountType.Corporate, 1000],
-  [AccountType.Trust, 1000],
-  [AccountType.Beneficiary, MINIMUM_INVESTMENT_AMOUNT],
+  [AccountType.Individual, ONE_TIME_INVESTMENT_MIN_AMOUNT],
+  [AccountType.Corporate, ONE_TIME_CORPORATION_MIN_AMOUNT],
+  [AccountType.Trust, ONE_TIME_CORPORATION_MIN_AMOUNT],
+  [AccountType.Beneficiary, ONE_TIME_INVESTMENT_MIN_AMOUNT],
 ]);
