1ca21fff1b5c20ed16c599a538c7490b3f7ee66c
adammalej <adammalej1@gmail.com>
2023-05-31 17:14:10 +0200

    fix: upgrade amounts

--- src/constants/investment-amounts.ts
+++ src/constants/investment-amounts.ts
@@ -16,6 +16,13 @@ const INDIVIDUAL_AMOUNTS = [
   { value: '10000', label: '$10k' },
 ];
 
+const RECURRING_INDIVIDUAL_AMOUNTS = [
+  { value: '100', label: '$100' },
+  { value: '500', label: '$500' },
+  { value: '1000', label: '$1,000' },
+  { value: '5000', label: '$5k' },
+];
+
 const CORPORATE_AND_TRUST_AMOUNTS = [
   { value: '1000', label: '$1,000' },
   { value: '5000', label: '$5,000' },
@@ -23,9 +30,23 @@ const CORPORATE_AND_TRUST_AMOUNTS = [
   { value: '20000', label: '$20k' },
 ];
 
+const RECURRING_CORPORATE_AND_TRUST_AMOUNTS = [
+  { value: '500', label: '$500' },
+  { value: '1000', label: '$1,000' },
+  { value: '5000', label: '$5k' },
+  { value: '10000', label: '$10k' },
+];
+
 export const INVESTMENT_PRESET_AMOUNTS: InvestmentsAmounts = {
   [AccountType.Individual]: INDIVIDUAL_AMOUNTS,
   [AccountType.Beneficiary]: BENEFICIARY_AMOUNTS,
   [AccountType.Corporate]: CORPORATE_AND_TRUST_AMOUNTS,
   [AccountType.Trust]: CORPORATE_AND_TRUST_AMOUNTS,
+};
+
+export const RECURRING_INVESTMENT_PRESET_AMOUNTS: InvestmentsAmounts = {
+  [AccountType.Individual]: RECURRING_INDIVIDUAL_AMOUNTS,
+  [AccountType.Beneficiary]: BENEFICIARY_AMOUNTS,
+  [AccountType.Corporate]: RECURRING_CORPORATE_AND_TRUST_AMOUNTS,
+  [AccountType.Trust]: RECURRING_CORPORATE_AND_TRUST_AMOUNTS,
 };
