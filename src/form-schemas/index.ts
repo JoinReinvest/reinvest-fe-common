76f0cdfab48c3e4d7b3cd32377ebc95f1e2d496b
Michal Gasiorek <michal@hookooekoo.co>
2023-06-05 12:57:39 +0200

    RIA-1296: forbid strings with just empty spaces

--- src/form-schemas/index.ts
+++ src/form-schemas/index.ts
@@ -4,9 +4,10 @@ import dayjs from 'dayjs';
 import zod from 'zod';
 
 const requiredError = 'This field is required';
-const standardRequiredString = zod.string().min(1, requiredError);
+const standardRequiredString = zod.string().trim().min(1, requiredError);
 const passwordSchema = zod
   .string({ required_error: requiredError })
+  .trim()
   .min(8, 'At least 8 characters are required')
   .regex(/[a-z]/, { message: 'At least one lowercase letter is required' })
   .regex(/[A-Z]/, { message: 'At least one uppercase letter is required' })
@@ -26,32 +27,40 @@ export const formValidationRules = {
   birthCountry: standardRequiredString,
   citizenshipCountry: standardRequiredString,
   visaType: standardRequiredString,
-  middleName: zod.string().optional(),
+  middleName: zod.string().trim().optional(),
   // The API expects the authentication code to be unformatted (ex: 251-529 -> 251529)
-  referralCode: zod.string().regex(/^[a-zA-Z0-9]{6}$/, { message: 'Invalid referral code' }),
+  referralCode: zod
+    .string()
+    .trim()
+    .regex(/^[a-zA-Z0-9]{6}$/, { message: 'Invalid referral code' }),
   date: zod.string({ required_error: requiredError }).regex(/^(\d{2})\/(\d{2})\/(\d{4})$/),
   phoneNumber: zod.string().regex(/^[0-9]{9,10}$/, { message: 'Invalid phone number' }),
   // The API expectes the authentication code to be unformatted (ex: 123-456 -> 123456)
   authenticationCode: zod.string({ required_error: requiredError }).regex(/^[a-zA-Z0-9]{6}$/, { message: 'Invalid authentication code' }),
   socialSecurityNumber: zod
     .string()
+    .trim()
     .regex(/^((?!666|000|9\d{2})\d{3}-(?!00)\d{2}-(?!0{4})\d{4})|(\*{3}-\*{2}-\*{2}(?!0{2})\d{2})$/, { message: 'Invalid Social Security Number' }),
   maskedSocialSecurityNumber: zod
     .string()
+    .trim()
     .regex(/^((?!666|000|9\d{2})\d{3}-(?!00)\d{2}-(?!0{4})\d{4})|(\*{3}-\*{2}-\d{4})$/, { message: 'Invalid Social Security Number' }),
   ein: zod.string().regex(/^[0-9]{2}-[0-9]{7}/, { message: 'Invalid EIN' }),
 
   address: zod.object({
     addressLine1: standardRequiredString,
-    addressLine2: zod.string().nullable().optional(),
+    addressLine2: zod.string().trim().nullable().optional(),
     city: standardRequiredString,
     state: zod.enum(STATE_CODES),
     // eslint-disable-next-line security/detect-unsafe-regex
-    zip: zod.string().regex(/^\d{5}(?:-\d{4})?$/, { message: 'Invalid zip code' }),
+    zip: zod
+      .string()
+      .trim()
+      .regex(/^\d{5}(?:-\d{4})?$/, { message: 'Invalid zip code' }),
   }),
-  symbolTicker: standardRequiredString.trim().nonempty('Please enter a ticker symbol'),
-  finraInstitutionName: standardRequiredString.trim().nonempty('Please enter your institution name'),
-  seniorPoliticalFigure: standardRequiredString.trim().nonempty('Please enter a political figure'),
+  symbolTicker: standardRequiredString.nonempty('Please enter a ticker symbol'),
+  finraInstitutionName: standardRequiredString.nonempty('Please enter your institution name'),
+  seniorPoliticalFigure: standardRequiredString.nonempty('Please enter a political figure'),
 };
 
 export const dateOlderThanEighteenYearsSchema = formValidationRules.date.superRefine((value, context) => {
