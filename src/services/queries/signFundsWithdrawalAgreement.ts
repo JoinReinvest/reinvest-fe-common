f66bbf31dd29e659303f45a7d2069ead872bf162
Michal Gasiorek <michal@hookooekoo.co>
2023-06-15 13:00:13 +0200

    RIA-1370: implement signFundsWithdrawalAgreement mutaion

---
+++ src/services/queries/signFundsWithdrawalAgreement.ts
@@ -0,0 +1,29 @@
+import { useMutation } from '@tanstack/react-query';
+import { gql, GraphQLClient } from 'graphql-request';
+
+import { Mutation } from '../../types/graphql';
+import { UseApiMutationWithParams } from './interfaces';
+
+type Parameters = { accountId: string };
+type Hook = UseApiMutationWithParams<'signFundsWithdrawalAgreement', Parameters>;
+
+const signFundsWithdrawalAgreementMutation = gql`
+  mutation signFundsWithdrawalAgreement($accountId: ID!) {
+    signFundsWithdrawalAgreement(accountId: $accountId) {
+      id
+      createdAt
+      signedAt
+    }
+  }
+`;
+
+export const useSignFundsWithdrawalAgreement: Hook = getApiClient =>
+  useMutation({
+    mutationFn: async accountId => {
+      const api = (await getApiClient()) as GraphQLClient;
+
+      const { signFundsWithdrawalAgreement } = await api.request<Mutation>(signFundsWithdrawalAgreementMutation, { accountId });
+
+      return signFundsWithdrawalAgreement;
+    },
+  });
