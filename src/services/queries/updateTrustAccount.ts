6e7a490fbe8fa07c74fe82e577b2a6e2b12b6233
Michal Gasiorek <michal@hookooekoo.co>
2023-06-16 14:22:55 +0200

    RIA-1460: implement update corporate and trust accounts mutation

---
+++ src/services/queries/updateTrustAccount.ts
@@ -0,0 +1,35 @@
+import { MutationUpdateTrustAccountArgs } from '../../types/graphql';
+import { useMutation } from '@tanstack/react-query';
+import { gql } from 'graphql-request';
+
+import { Mutation } from '../../types/graphql';
+import { UseApiMutationWithParams } from './interfaces';
+import { TrustAccountDetailsFragment } from './fragments/trustAccountDetails';
+
+type Hook = UseApiMutationWithParams<'updateTrustAccount', MutationUpdateTrustAccountArgs>;
+
+const updateTrustAccountMutation = gql`
+  ${TrustAccountDetailsFragment}
+  mutation updateTrustAccount($accountId: ID!, $input: UpdateCompanyAccountInput) {
+    updateTrustAccount(accountId: $accountId, input: $input) {
+      details {
+        ...TrustAccountDetailsFragment
+      }
+    }
+  }
+`;
+
+export const useUpdateTrustAccount: Hook = getApiClient =>
+  useMutation({
+    mutationFn: async input => {
+      const api = await getApiClient();
+
+      if (!api) {
+        return null;
+      }
+
+      const { updateTrustAccount } = await api.request<Mutation>(updateTrustAccountMutation, { ...input });
+
+      return updateTrustAccount;
+    },
+  });
