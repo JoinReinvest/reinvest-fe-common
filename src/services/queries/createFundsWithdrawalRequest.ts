f41123080ab4fa17f48545307b063407f1e1accc
reinny-hkek <reinny@hookooekoo.co>
2023-06-20 12:22:11 +0200

    feat: providing the reason for the funds withdrawal request

--- src/services/queries/createFundsWithdrawalRequest.ts
+++ src/services/queries/createFundsWithdrawalRequest.ts
@@ -1,12 +1,11 @@
 import { useMutation } from '@tanstack/react-query';
 import { gql, GraphQLClient } from 'graphql-request';
 
-import { Mutation } from '../../types/graphql';
+import { Mutation, MutationCreateFundsWithdrawalRequestArgs } from '../../types/graphql';
 import { UseApiMutationWithParams } from './interfaces';
 import { FundsWithdrawalRequestFragment } from './fragments/fundsWithdrawalRequest';
 
-type Parameters = { accountId: string };
-type Hook = UseApiMutationWithParams<'createFundsWithdrawalRequest', Parameters>;
+type Hook = UseApiMutationWithParams<'createFundsWithdrawalRequest', MutationCreateFundsWithdrawalRequestArgs>;
 
 const createFundsWithdrawalRequestMutation = gql`
   ${FundsWithdrawalRequestFragment}
