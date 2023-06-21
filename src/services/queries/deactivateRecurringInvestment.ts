93a93990db32bb8e9c3602393ac553583f00cf03
Michal Gasiorek <michal@hookooekoo.co>
2023-06-21 13:18:03 +0200

    RIA-1487: implement deactivateRecurringInvestment mutation

---
+++ src/services/queries/deactivateRecurringInvestment.ts
@@ -0,0 +1,25 @@
+import { MutationDeactivateRecurringInvestmentArgs } from './../../types/graphql';
+import { useMutation } from '@tanstack/react-query';
+import { gql, GraphQLClient } from 'graphql-request';
+
+import { Mutation } from '../../types/graphql';
+import { UseApiMutationWithParams } from './interfaces';
+
+type Hook = UseApiMutationWithParams<'deactivateRecurringInvestment', MutationDeactivateRecurringInvestmentArgs>;
+
+const deactivateRecurringInvestmentMutation = gql`
+  mutation deactivateRecurringInvestment($accountId: ID!) {
+    deactivateRecurringInvestment(accountId: $accountId)
+  }
+`;
+
+export const useDeactivateRecurringInvestment: Hook = getApiClient =>
+  useMutation({
+    mutationFn: async input => {
+      const api = (await getApiClient()) as GraphQLClient;
+
+      const { deactivateRecurringInvestment } = await api.request<Mutation>(deactivateRecurringInvestmentMutation, { ...input });
+
+      return deactivateRecurringInvestment;
+    },
+  });
