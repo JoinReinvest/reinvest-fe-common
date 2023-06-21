f08965e88df9ffdd9402bbb9a6154147cbf54dcb
Michal Gasiorek <michal@hookooekoo.co>
2023-06-21 13:28:48 +0200

    RIA-1487: implement unsuspendRecurringInvestment mutation

---
+++ src/services/queries/unsuspendRecurringInvestment.ts
@@ -0,0 +1,25 @@
+import { MutationUnsuspendRecurringInvestmentArgs } from './../../types/graphql';
+import { useMutation } from '@tanstack/react-query';
+import { gql, GraphQLClient } from 'graphql-request';
+
+import { Mutation } from '../../types/graphql';
+import { UseApiMutationWithParams } from './interfaces';
+
+type Hook = UseApiMutationWithParams<'unsuspendRecurringInvestment', MutationUnsuspendRecurringInvestmentArgs>;
+
+const unsuspendRecurringInvestmentMutation = gql`
+  mutation unsuspendRecurringInvestment($accountId: ID!) {
+    unsuspendRecurringInvestment(accountId: $accountId)
+  }
+`;
+
+export const useUnsuspendRecurringInvestment: Hook = getApiClient =>
+  useMutation({
+    mutationFn: async input => {
+      const api = (await getApiClient()) as GraphQLClient;
+
+      const { unsuspendRecurringInvestment } = await api.request<Mutation>(unsuspendRecurringInvestmentMutation, { ...input });
+
+      return unsuspendRecurringInvestment;
+    },
+  });
