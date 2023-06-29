b3fbe3ee3a0940dea220c80aeeffd4510d579106
adammalej <adammalej1@gmail.com>
2023-06-29 14:33:35 +0200

    feat: add experience titles

--- src/constants/experiences.ts
+++ src/constants/experiences.ts
@@ -1,11 +1,18 @@
 import { Experience } from '../types/graphql';
 import { SelectCardOption } from '../types/select-card-option';
 
+export const EXPERIENCE_TITLES = {
+  [Experience.NoExperience]: 'No Experience',
+  [Experience.SomeExperience]: 'Some Experience',
+  [Experience.VeryExperienced]: 'Very Experienced',
+  [Experience.Expert]: 'Expert',
+}
+
 export const EXPERIENCES_AS_OPTIONS: SelectCardOption[] = [
-  { title: 'No Experience', value: Experience.NoExperience },
-  { title: 'Some Experience', value: Experience.SomeExperience },
-  { title: 'Very Experienced', value: Experience.VeryExperienced },
-  { title: 'Expert', value: Experience.Expert },
+  { title: EXPERIENCE_TITLES[Experience.NoExperience], value: Experience.NoExperience },
+  { title: EXPERIENCE_TITLES[Experience.SomeExperience], value: Experience.SomeExperience },
+  { title: EXPERIENCE_TITLES[Experience.VeryExperienced], value: Experience.VeryExperienced },
+  { title: EXPERIENCE_TITLES[Experience.Expert], value: Experience.Expert },
 ];
 
 export const EXPERIENCES_AS_VALUES = [Experience.NoExperience, Experience.SomeExperience, Experience.VeryExperienced, Experience.Expert] as const;
